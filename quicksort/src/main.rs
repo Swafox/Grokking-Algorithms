992d1bb008c87830b50855b40c9b57202dcde2cf
Swafox <swafox@pm.me>
2024-08-11 13:30:53 +0300

    Add random pick pivot for quicksort

--- quicksort/src/main.rs
+++ quicksort/src/main.rs
@@ -1,4 +1,6 @@
-fn quicksort(mut list: Vec<i32>) -> Vec<i32> {
+use rand::Rng;
+
+fn quicksort_mid(mut list: Vec<i32>) -> Vec<i32> {
     if list.len() < 2 {
         return list;
     }
@@ -18,15 +20,45 @@ fn quicksort(mut list: Vec<i32>) -> Vec<i32> {
         }
     }
 
-    let mut result: Vec<i32> = quicksort(left);
+    let mut result: Vec<i32> = quicksort_mid(left);
     result.push(pivot);
-    result.extend(quicksort(right));
+    result.extend(quicksort_mid(right));
+
+    result
+}
+
+fn quicksort_random(mut list: Vec<i32>) -> Vec<i32> {
+    if list.len() < 2 {
+        return list;
+    }
+
+    let pivot: i32 = list.remove(rand::thread_rng().gen_range(0..list.len() as i32) as usize);
+
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    for item in list {
+        if item <= pivot {
+            left.push(item);
+        } else {
+            right.push(item);
+        }
+    }
+
+    let mut result: Vec<i32> = quicksort_random(left);
+    result.push(pivot);
+    result.extend(quicksort_random(right));
 
     result
 }
 
 fn main() {
     let input: Vec<i32> = vec![1, 3, 2, 5, 6, 4, 9, 8, 7];
-    let sorted: Vec<i32> = quicksort(input);
+    let input2: Vec<i32> = input.clone();
+
+    let sorted: Vec<i32> = quicksort_mid(input);
+    let sorted2: Vec<i32> = quicksort_random(input2);
+
     println!("{:?}", sorted);
+    println!("{:?}", sorted2);
 }

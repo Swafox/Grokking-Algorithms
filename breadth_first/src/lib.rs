18b65a525cebf0c5a952c1f0d1f97dc391c026ae
Swafox <swafox@pm.me>
2024-08-25 13:06:15 +0300

    Switch to String vectors and perfomance test

--- breadth_first/src/lib.rs
+++ breadth_first/src/lib.rs
@@ -4,19 +4,19 @@ fn person_is_seller(name: &str) -> bool {
     name.ends_with('m')
 }
 
-pub fn breadth_first_search(graph: HashMap<&str, Vec<&str>>, start: &str) -> bool {
-    let mut search_queue: VecDeque<&str> = VecDeque::new();
-    search_queue.push_back(start);
-    let mut searched: HashSet<&str> = HashSet::new();
+pub fn breadth_first_search(graph: HashMap<String, Vec<String>>, start: &str) -> bool {
+    let mut search_queue: VecDeque<String> = VecDeque::new();
+    search_queue.push_back(start.to_string());
+    let mut searched: HashSet<String> = HashSet::new();
 
     while let Some(person) = search_queue.pop_front() {
-        if !searched.contains(person) {
-            if person_is_seller(person) {
+        if !searched.contains(&person) {
+            if person_is_seller(&person) {
                 println!("{} is a mango seller!", person);
                 return true;
             }
-            if let Some(neighbors) = graph.get(person) {
-                search_queue.extend(neighbors);
+            if let Some(neighbors) = graph.get(&person) {
+                search_queue.extend(neighbors.iter().map(|n| n.to_string()));
             }
             searched.insert(person);
         }
--- breadth_first/tests/spec.rs
+++ breadth_first/tests/spec.rs
@@ -3,20 +3,51 @@ mod tests {
     use breadth_first::breadth_first_search;
 
     use std::collections::HashMap;
+    use std::time::Instant;
 
     #[test]
     fn test_breadth_first_search() {
-        let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
+        let mut graph: HashMap<String, Vec<String>> = HashMap::new();
 
-        graph.insert("you", vec!["alice", "bob", "claire"]);
-        graph.insert("bob", vec!["anuj", "peggy"]);
-        graph.insert("alice", vec!["peggy"]);
-        graph.insert("claire", vec!["thom", "jonny"]);
-        graph.insert("anuj", Vec::new());
-        graph.insert("peggy", Vec::new());
-        graph.insert("thom", Vec::new());
-        graph.insert("jonny", Vec::new());
+        graph.insert(
+            "you".to_string(),
+            vec!["alice".to_string(), "bob".to_string(), "claire".to_string()],
+        );
+        graph.insert(
+            "bob".to_string(),
+            vec!["anuj".to_string(), "peggy".to_string()],
+        );
+        graph.insert("alice".to_string(), vec!["peggy".to_string()]);
+        graph.insert(
+            "claire".to_string(),
+            vec!["thom".to_string(), "jonny".to_string()],
+        );
+        graph.insert("anuj".to_string(), Vec::new());
+        graph.insert("peggy".to_string(), Vec::new());
+        graph.insert("thom".to_string(), Vec::new());
+        graph.insert("jonny".to_string(), Vec::new());
 
         assert!(breadth_first_search(graph, "you"));
+    }
+
+    #[test]
+    fn test_breadth_first_search_performance() {
+        let mut graph: HashMap<String, Vec<String>> = HashMap::new();
+
+        for i in 0..1000 {
+            let key: String = i.to_string();
+            let values: Vec<String> = (1000..2000).map(|j| j.to_string()).collect();
+            graph.insert(key, values);
+        }
+
+        let start_time: Instant = Instant::now();
+        let result: bool = breadth_first_search(graph, "0");
+        let duration: std::time::Duration = start_time.elapsed();
+
+        println!(
+            "Breadth-first search took {:.6} seconds",
+            duration.as_secs_f64()
+        );
+        assert!(!result);
     }
 }

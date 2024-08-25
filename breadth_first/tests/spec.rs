10f880871bcc180d3428708e72be3eca111e7ad1
Swafox <swafox@pm.me>
2024-08-25 13:09:41 +0300

    Add test case with 10k nodes

--- breadth_first/tests/spec.rs
+++ breadth_first/tests/spec.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use breadth_first::breadth_first_search;
-
+    use rand::Rng;
     use std::collections::HashMap;
     use std::time::Instant;
 
@@ -49,5 +49,35 @@ mod tests {
             duration.as_secs_f64()
         );
         assert!(!result);
+    }
+
+    #[test]
+    fn test_breadth_first_search_large_random_graph() {
+        let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
+        let mut graph: HashMap<String, Vec<String>> = HashMap::new();
+        let node_count: i32 = 100000;
+        let max_edges_per_node: i32 = 50;
+
+        // Generate random graph
+        for i in 0..node_count {
+            let key: String = i.to_string();
+            let edge_count: i32 = rng.gen_range(0..max_edges_per_node);
+            let values: Vec<String> = (0..edge_count)
+                .map(|_| rng.gen_range(0..node_count).to_string())
+                .collect();
+            graph.insert(key, values);
+        }
+
+        let start_node: String = "0".to_string();
+        let start_time: Instant = Instant::now();
+        let result: bool = breadth_first_search(graph, &start_node);
+        let duration: std::time::Duration = start_time.elapsed();
+
+        println!(
+            "Breadth-first search on a random graph with {} nodes took {:.6} seconds",
+            node_count,
+            duration.as_secs_f64()
+        );
+        println!("Result: {}", result);
     }
 }
